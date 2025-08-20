cc57148d826ce357452f7d5e5bb0b26d51c5b7cb
Mostakem Mia <155542883+Mostakem-mia@users.noreply.github.com>
2025-08-20 23:07:01 +0600

    Add files via upload

---
+++ Cafe-Shop-Management-System/components/footer.php
@@ -0,0 +1,82 @@
+ <footer class="footer">
+
+    <section class="grid">
+
+       <div class="box">
+
+          <h3>quick links</h3>
+
+          <a href="home.php"> Home </a>
+          <a href="menu.php"> Menu </a>
+          <a href="orders.php"> Order </a>
+          <a href="about.php"> About </a>
+       </div>
+
+       <div class="box">
+          <h3> Cafe Shop <i class="fas fa-shopping-basket"></i> </h3>
+          <div class="share">
+             <a href="#" class="fab fa-facebook-f"></a>
+             <a href="#" class="fab fa-twitter"></a>
+             <a href="#" class="fab fa-instagram"></a>
+             <a href="#" class="fab fa-linkedin"></a>
+          </div>
+       </div>
+
+       <div class="box">
+
+          <h3>Opening Hours</h3>
+          <div class="date">
+             <p>MONDAY </p> <samp>9.00 - 22.00</samp>
+          </div>
+          <div class="date">
+             <p>TUESDAY </p> <samp>9.00 - 22.00</samp>
+          </div>
+          <div class="date">
+             <p>WEDNESDAY </p><samp>CLOSED</samp>
+          </div>
+          <div class="date">
+             <p>THURSDAY </p><samp>9.00 - 22.00</samp>
+          </div>
+          <div class="date">
+             <p>FRIDAY </p> <samp>9.00 - 22.00</samp>
+          </div>
+          <div class="date">
+             <p>SATURDAY </p> <samp>9.00 - 22.00</samp>
+          </div>
+          <div class="date">
+             <p>SUNDAY </p><samp>9.00 - 22.00</samp>
+          </div>
+
+
+       </div>
+
+       <div class="box">
+          <h3>contact info</h3>
+          <div class="phone">
+             <p> <i class="fas fa-phone"></i> +88015 0000 0000 </p>
+             <p> <i class="fas fa-phone"></i> +88017 0000 0000 </a>
+             <p> <i class="fas fa-envelope"></i> cafe.shop@gmail.com </p>
+          </div>
+
+          <div class="phone1">
+             <h3>Branch Location</h3>
+
+             <p> <i class="fas fa-map-marker-alt"></i> Index Plaza, Station Road, Narsingdi, Dhaka </p>
+             <p> <i class="fas fa-envelope"></i> cafe.shop@gmail.com </p>
+          </div>
+       </div>
+
+    </section>
+
+    <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span></span> | <a>Fall-2024 CSE302 (Group:5)</a> | all rights reserved!</div>
+
+ </footer>
+
+
+
+
+ <!-- footer section starts 
+
+
+
+footer section ends -->
